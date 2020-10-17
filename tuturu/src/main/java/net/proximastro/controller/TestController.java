568e954668750cc5953c63a27eee24c35cecf018
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2020-10-17 15:25:18 +0200

    NEW : Ajout d'un controller de test

--- tuturu/src/main/java/net/proximastro/controller/TestController.java
+++ tuturu/src/main/java/net/proximastro/controller/TestController.java
@@ -1,4 +1,12 @@
 package net.proximastro.controller;
 
+import net.proximastro.app.RouteController;
+
+import java.util.HashMap;
+
 public class TestController extends RouteController {
+
+    public TestController(){
+
+    }
 }
